a9e68692f89a31c88dfdeca99b53d9081d52bb09
bigrushdog <randall.rushing@gmail.com>
2016-10-15 19:11:42 +0200

    DUI: Initial N checkin
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/blackbox/settings/fragments/NavigationBarSettings.java
+++ src/com/blackbox/settings/fragments/NavigationBarSettings.java
@@ -1,48 +1,49 @@
 package com.blackbox.settings.fragments;
 
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.os.UserManager;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
+import android.provider.Settings;
+
+import com.android.settings.R;
+import com.android.settings.SeekBarPreference;
+import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.ActivityNotFoundException;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.os.Bundle;
-import android.os.RemoteException;
-import android.os.PowerManager;
-import android.preference.CheckBoxPreference;
-import android.preference.ListPreference;
-import android.preference.Preference;
-import android.preference.PreferenceFragment;
-import android.preference.Preference.OnPreferenceChangeListener;
-import android.preference.PreferenceCategory;
-import android.preference.PreferenceManager;
-import android.preference.PreferenceScreen;
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
-import android.preference.SwitchPreference;
-import android.provider.Settings;
-import com.android.settings.R;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-
-import com.android.settings.SettingsPreferenceFragment;
-
-
-public class NavigationBarSettings extends SettingsPreferenceFragment {
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        addPreferencesFromResource(R.xml.blackbox_settings_navigation);
-    }
+public class NavigationBarSettings extends SettingsPreferenceFragment
+        implements OnPreferenceChangeListener {
 
     @Override
     protected int getMetricsCategory() {
         return MetricsEvent.BLACKBOX_SETTINGS;
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.blackbox_settings_navigation);
+
+        final Resources res = getActivity().getResources();
+
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
 }
+
