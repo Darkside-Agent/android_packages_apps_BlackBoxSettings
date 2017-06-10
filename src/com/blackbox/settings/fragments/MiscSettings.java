f7488362f0b86bbe13df27974963ba57581a92e7
dsa <darksideagent@gmail.com>
2017-06-10 23:37:03 +0100

    Add Expanded Desktop

--- src/com/blackbox/settings/fragments/MiscSettings.java
+++ src/com/blackbox/settings/fragments/MiscSettings.java
@@ -1,30 +1,20 @@
 package com.blackbox.settings.fragments;
 
-import com.android.internal.logging.MetricsProto.MetricsEvent;
-
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
 import android.content.Context;
-import android.content.ContentResolver;
-import android.content.DialogInterface;
-import android.content.res.Resources;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.SystemProperties;
-import android.os.UserHandle;
-import android.preference.ListPreference;
-import android.preference.Preference;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
-import android.preference.Preference.OnPreferenceChangeListener;
-import android.preference.SwitchPreference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
+
+import com.android.internal.logging.MetricsProto.MetricsEvent;
 import com.android.settings.DevelopmentSettings;
 import com.android.settings.R;
-
-import java.util.Arrays;
-import java.util.HashSet;
-
 import com.android.settings.SettingsPreferenceFragment;
 
 public class MiscSettings extends SettingsPreferenceFragment implements
@@ -57,8 +47,8 @@ public class MiscSettings extends SettingsPreferenceFragment implements
     }
 
     @Override
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
-         if (preference == mScrollingCachePref) {
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mScrollingCachePref) {
             if (newValue != null) {
             String ScrollingCache = (String) newValue;
             SystemProperties.set(SCROLLINGCACHE_PERSIST_PROP, ScrollingCache);
