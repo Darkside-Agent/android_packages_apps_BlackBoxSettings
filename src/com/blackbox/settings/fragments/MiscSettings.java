94846c5b6077cdaa931f8176d075059df12d8a3a
dsa <darksideagent@gmail.com>
2017-06-15 20:48:32 +0100

    Move scrolling cache into animations

--- src/com/blackbox/settings/fragments/MiscSettings.java
+++ src/com/blackbox/settings/fragments/MiscSettings.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.SystemProperties;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
@@ -22,11 +21,6 @@ public class MiscSettings extends SettingsPreferenceFragment implements
 
     private static final String KEY_LOCK_CLOCK = "lock_clock";
     private static final String KEY_LOCK_CLOCK_PACKAGE_NAME = "com.cyanogenmod.lockclock";
-    private static final String SCROLLINGCACHE_PREF = "pref_scrollingcache";
-    private static final String SCROLLINGCACHE_PERSIST_PROP = "persist.sys.scrollingcache";
-    private static final String SCROLLINGCACHE_DEFAULT = "1";
-
-    private ListPreference mScrollingCachePref;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -39,27 +33,12 @@ public class MiscSettings extends SettingsPreferenceFragment implements
             getPreferenceScreen().removePreference(findPreference(KEY_LOCK_CLOCK));
         }
 
-        mScrollingCachePref = (ListPreference) findPreference(SCROLLINGCACHE_PREF);
-        mScrollingCachePref.setValue(SystemProperties.get(SCROLLINGCACHE_PERSIST_PROP,
-                SystemProperties.get(SCROLLINGCACHE_PERSIST_PROP, SCROLLINGCACHE_DEFAULT)));
-        mScrollingCachePref.setSummary(mScrollingCachePref.getEntry());
-        mScrollingCachePref.setOnPreferenceChangeListener(this);
     }
 
     @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mScrollingCachePref) {
-            if (newValue != null) {
-            String ScrollingCache = (String) newValue;
-            SystemProperties.set(SCROLLINGCACHE_PERSIST_PROP, ScrollingCache);
-            int ScrollingCacheIndex = mScrollingCachePref
-                    .findIndexOfValue(ScrollingCache);
-            mScrollingCachePref
-                    .setSummary(mScrollingCachePref.getEntries()[ScrollingCacheIndex]);
-            }
-            return true;
-        }
-        return false;
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+
+    return false;
     }
 
     @Override
