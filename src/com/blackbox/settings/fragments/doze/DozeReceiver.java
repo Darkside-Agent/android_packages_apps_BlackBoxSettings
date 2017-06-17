crdroidandroid/android_packages_apps_crDroidSettings

Doze: Add gesture support for tilt, pickup, proximity sensors [2/2]
Change-Id: Ide9bf3be4fddb6945375561b65ff082fdd007087
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/com/crdroid/settings/fragments/doze/DozeReceiver.java
@@ -1,0 +1,38 @@
+/*
+ * Copyright (C) 2017 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.crdroid.settings.fragments.doze;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class DozeReceiver extends BroadcastReceiver {
+
+    private static final boolean DEBUG = false;
+    private static final String TAG = "crDroidDoze";
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+            if (Utils.isDozeEnabled(context)) {
+                if (DEBUG) Log.d(TAG, "Starting service");
+                Utils.startService(context);
+            }
+        }
+    }
+}

src/com/crdroid/settings/fragments/doze/DozeService.java
@@ -1,0 +1,105 @@
+/*
+ * Copyright (C) 2017 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.crdroid.settings.fragments.doze;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.IBinder;
+import android.util.Log;
+
+public class DozeService extends Service {
+    private static final String TAG = "crDroidDozeService";
+    private static final boolean DEBUG = false;
+
+    private ProximitySensor mProximitySensor;
+    private TiltSensor mTiltSensor;
+    private PickupSensor mPickupSensor;
+    private BroadcastReceiver mScreenStateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
+                onDisplayOn();
+            } else if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+                onDisplayOff();
+            }
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        if (DEBUG) Log.d(TAG, "Creating service");
+        mProximitySensor = new ProximitySensor(this);
+        mTiltSensor = new TiltSensor(this);
+        mPickupSensor = new PickupSensor(this);
+
+        IntentFilter screenStateFilter = new IntentFilter(Intent.ACTION_SCREEN_ON);
+        screenStateFilter.addAction(Intent.ACTION_SCREEN_OFF);
+        registerReceiver(mScreenStateReceiver, screenStateFilter);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (DEBUG) Log.d(TAG, "Starting service");
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        if (DEBUG) Log.d(TAG, "Destroying service");
+        super.onDestroy();
+        this.unregisterReceiver(mScreenStateReceiver);
+        mProximitySensor.disable();
+        mTiltSensor.disable();
+        mPickupSensor.disable();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    private void onDisplayOn() {
+        if (DEBUG) Log.d(TAG, "Display on");
+        if (Utils.tiltEnabled(this)) {
+            mTiltSensor.disable();
+        }
+        if (Utils.pickUpEnabled(this)) {
+            mPickupSensor.disable();
+        }
+        if (Utils.handwaveGestureEnabled(this) ||
+                Utils.pocketGestureEnabled(this)) {
+            mProximitySensor.disable();
+        }
+    }
+
+    private void onDisplayOff() {
+        if (DEBUG) Log.d(TAG, "Display off");
+        if (Utils.tiltEnabled(this)) {
+            mTiltSensor.enable();
+        }
+        if (Utils.pickUpEnabled(this)) {
+            mPickupSensor.enable();
+        }
+        if (Utils.handwaveGestureEnabled(this) ||
+                Utils.pocketGestureEnabled(this)) {
+            mProximitySensor.enable();
+        }
+    }
+}

src/com/crdroid/settings/fragments/doze/ProximitySensor.java
@@ -1,0 +1,90 @@
+/*
+ * Copyright (C) 2017 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.crdroid.settings.fragments.doze;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.util.Log;
+
+public class ProximitySensor implements SensorEventListener {
+
+    private static final boolean DEBUG = false;
+    private static final String TAG = "ProximitySensor";
+
+    // Maximum time for the hand to cover the sensor: 1s
+    private static final int HANDWAVE_MAX_DELTA_NS = 1000 * 1000 * 1000;
+
+    // Minimum time until the device is considered to have been in the pocket: 2s
+    private static final int POCKET_MIN_DELTA_NS = 2000 * 1000 * 1000;
+
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+    private Context mContext;
+
+    private boolean mSawNear = false;
+    private long mInPocketTime = 0;
+
+    public ProximitySensor(Context context) {
+        mContext = context;
+        mSensorManager = (SensorManager) mContext.getSystemService(Context.SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        boolean isNear = event.values[0] < mSensor.getMaximumRange();
+        if (mSawNear && !isNear) {
+            if (shouldPulse(event.timestamp)) {
+                Utils.launchDozePulse(mContext);
+            }
+        } else {
+            mInPocketTime = event.timestamp;
+        }
+        mSawNear = isNear;
+    }
+
+    private boolean shouldPulse(long timestamp) {
+        long delta = timestamp - mInPocketTime;
+
+        if (Utils.handwaveGestureEnabled(mContext) && Utils.pocketGestureEnabled(mContext)) {
+            return true;
+        } else if (Utils.handwaveGestureEnabled(mContext)) {
+            return delta < HANDWAVE_MAX_DELTA_NS;
+        } else if (Utils.pocketGestureEnabled(mContext)) {
+            return delta >= POCKET_MIN_DELTA_NS;
+        }
+        return false;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        /* Empty */
+    }
+
+    protected void enable() {
+        if (DEBUG) Log.d(TAG, "Enabling");
+        mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void disable() {
+        if (DEBUG) Log.d(TAG, "Disabling");
+        mSensorManager.unregisterListener(this, mSensor);
+    }
+}

src/com/crdroid/settings/fragments/doze/TiltSensor.java
@@ -1,0 +1,79 @@
+/*
+ * Copyright (C) 2017 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.crdroid.settings.fragments.doze;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.SystemClock;
+import android.util.Log;
+
+public class TiltSensor implements SensorEventListener {
+
+    private static final boolean DEBUG = false;
+    private static final String TAG = "TiltSensor";
+
+    private static final int BATCH_LATENCY_IN_MS = 100;
+    private static final int MIN_PULSE_INTERVAL_MS = 2500;
+
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+    private Context mContext;
+
+    private long mEntryTimestamp;
+
+    public TiltSensor(Context context) {
+        mContext = context;
+        mSensorManager = (SensorManager) mContext.getSystemService(Context.SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_TILT_DETECTOR);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (DEBUG) Log.d(TAG, "Got sensor event: " + event.values[0]);
+
+        long delta = SystemClock.elapsedRealtime() - mEntryTimestamp;
+        if (delta < MIN_PULSE_INTERVAL_MS) {
+            return;
+        } else {
+            mEntryTimestamp = SystemClock.elapsedRealtime();
+        }
+
+        if (event.values[0] == 1) {
+            Utils.launchDozePulse(mContext);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        /* Empty */
+    }
+
+    protected void enable() {
+        if (DEBUG) Log.d(TAG, "Enabling");
+        mSensorManager.registerListener(this, mSensor,
+                SensorManager.SENSOR_DELAY_NORMAL, BATCH_LATENCY_IN_MS * 1000);
+        mEntryTimestamp = SystemClock.elapsedRealtime();
+    }
+
+    protected void disable() {
+        if (DEBUG) Log.d(TAG, "Disabling");
+        mSensorManager.unregisterListener(this, mSensor);
+    }
+}

src/com/crdroid/settings/fragments/doze/Utils.java
@@ -1,0 +1,89 @@
+/*
+ * Copyright (C) 2017 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.crdroid.settings.fragments.doze;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.util.Log;
+
+import static android.provider.Settings.Secure.DOZE_ENABLED;
+
+public final class Utils {
+
+    public static final Uri DOZE_ENABLED_URI = Settings.Secure.getUriFor(DOZE_ENABLED);
+    private static final String TAG = "DozeUtils";
+    private static final boolean DEBUG = false;
+    private static final String DOZE_INTENT = "com.android.systemui.doze.pulse";
+
+    protected static void startService(Context context) {
+        if (DEBUG) Log.d(TAG, "Starting service");
+        context.startService(new Intent(context, DozeService.class));
+    }
+
+    protected static void stopService(Context context) {
+        if (DEBUG) Log.d(TAG, "Stopping service");
+        context.stopService(new Intent(context, DozeService.class));
+    }
+
+    protected static boolean isDozeEnabled(Context context) {
+        return Settings.Secure.getInt(context.getContentResolver(),
+                DOZE_ENABLED, context.getResources().getBoolean(
+                com.android.internal.R.bool.config_doze_enabled_by_default) ? 1 : 0) != 0;
+    }
+
+    protected static void enableService(boolean enable, Context context) {
+        if (enable) {
+            startService(context);
+        } else {
+            stopService(context);
+        }
+    }
+
+    protected static void launchDozePulse(Context context) {
+        if (DEBUG) Log.d(TAG, "Launch doze pulse");
+        context.sendBroadcastAsUser(new Intent(DOZE_INTENT),
+                new UserHandle(UserHandle.USER_CURRENT));
+    }
+
+    protected static boolean tiltEnabled(Context context) {
+        return Settings.System.getInt(context.getContentResolver(),
+                Settings.System.DOZE_TRIGGER_TILT, 0) == 1;
+    }
+
+    protected static boolean pickUpEnabled(Context context) {
+        return Settings.System.getInt(context.getContentResolver(),
+                Settings.System.DOZE_TRIGGER_PICKUP, 0) == 1;
+    }
+
+    protected static boolean handwaveGestureEnabled(Context context) {
+        return Settings.System.getInt(context.getContentResolver(),
+                Settings.System.DOZE_TRIGGER_HANDWAVE, 0) == 1;
+    }
+
+    protected static boolean pocketGestureEnabled(Context context) {
+        return Settings.System.getInt(context.getContentResolver(),
+                Settings.System.DOZE_TRIGGER_POCKET, 0) == 1;
+    }
+
+    protected static boolean sensorsEnabled(Context context) {
+        return tiltEnabled(context) || pickUpEnabled(context) || handwaveGestureEnabled(context)
+                || pocketGestureEnabled(context);
+    }
+}

Darkside-Agent/android_packages_apps_BlackBoxSettings

src/com/blackbox/settings/fragments/doze/DozeReceiver.java
@@ -1,0 +1,38 @@
+/*
+ * Copyright (C) 2017 BlackBox Hybrid Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.blackbox.settings.fragments.doze;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class DozeReceiver extends BroadcastReceiver {
+
+    private static final boolean DEBUG = false;
+    private static final String TAG = "BlackBoxDoze";
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+            if (Utils.isDozeEnabled(context)) {
+                if (DEBUG) Log.d(TAG, "Starting service");
+                Utils.startService(context);
+            }
+        }
+    }
+}

src/com/blackbox/settings/fragments/doze/DozeService.java
@@ -1,0 +1,105 @@
+/*
+ * Copyright (C) 2017 BlackBox Hybrid Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.blackbox.settings.fragments.doze;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.IBinder;
+import android.util.Log;
+
+public class DozeService extends Service {
+    private static final String TAG = "BlackBoxDozeService";
+    private static final boolean DEBUG = false;
+
+    private ProximitySensor mProximitySensor;
+    private TiltSensor mTiltSensor;
+    private PickupSensor mPickupSensor;
+    private BroadcastReceiver mScreenStateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
+                onDisplayOn();
+            } else if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+                onDisplayOff();
+            }
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        if (DEBUG) Log.d(TAG, "Creating service");
+        mProximitySensor = new ProximitySensor(this);
+        mTiltSensor = new TiltSensor(this);
+        mPickupSensor = new PickupSensor(this);
+
+        IntentFilter screenStateFilter = new IntentFilter(Intent.ACTION_SCREEN_ON);
+        screenStateFilter.addAction(Intent.ACTION_SCREEN_OFF);
+        registerReceiver(mScreenStateReceiver, screenStateFilter);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (DEBUG) Log.d(TAG, "Starting service");
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        if (DEBUG) Log.d(TAG, "Destroying service");
+        super.onDestroy();
+        this.unregisterReceiver(mScreenStateReceiver);
+        mProximitySensor.disable();
+        mTiltSensor.disable();
+        mPickupSensor.disable();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    private void onDisplayOn() {
+        if (DEBUG) Log.d(TAG, "Display on");
+        if (Utils.tiltEnabled(this)) {
+            mTiltSensor.disable();
+        }
+        if (Utils.pickUpEnabled(this)) {
+            mPickupSensor.disable();
+        }
+        if (Utils.handwaveGestureEnabled(this) ||
+                Utils.pocketGestureEnabled(this)) {
+            mProximitySensor.disable();
+        }
+    }
+
+    private void onDisplayOff() {
+        if (DEBUG) Log.d(TAG, "Display off");
+        if (Utils.tiltEnabled(this)) {
+            mTiltSensor.enable();
+        }
+        if (Utils.pickUpEnabled(this)) {
+            mPickupSensor.enable();
+        }
+        if (Utils.handwaveGestureEnabled(this) ||
+                Utils.pocketGestureEnabled(this)) {
+            mProximitySensor.enable();
+        }
+    }
+}

src/com/blackbox/settings/fragments/doze/ProximitySensor.java
@@ -1,0 +1,90 @@
+/*
+ * Copyright (C) 2017 BlackBox Hybrid Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.blackbox.settings.fragments.doze;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.util.Log;
+
+public class ProximitySensor implements SensorEventListener {
+
+    private static final boolean DEBUG = false;
+    private static final String TAG = "ProximitySensor";
+
+    // Maximum time for the hand to cover the sensor: 1s
+    private static final int HANDWAVE_MAX_DELTA_NS = 1000 * 1000 * 1000;
+
+    // Minimum time until the device is considered to have been in the pocket: 2s
+    private static final int POCKET_MIN_DELTA_NS = 2000 * 1000 * 1000;
+
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+    private Context mContext;
+
+    private boolean mSawNear = false;
+    private long mInPocketTime = 0;
+
+    public ProximitySensor(Context context) {
+        mContext = context;
+        mSensorManager = (SensorManager) mContext.getSystemService(Context.SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        boolean isNear = event.values[0] < mSensor.getMaximumRange();
+        if (mSawNear && !isNear) {
+            if (shouldPulse(event.timestamp)) {
+                Utils.launchDozePulse(mContext);
+            }
+        } else {
+            mInPocketTime = event.timestamp;
+        }
+        mSawNear = isNear;
+    }
+
+    private boolean shouldPulse(long timestamp) {
+        long delta = timestamp - mInPocketTime;
+
+        if (Utils.handwaveGestureEnabled(mContext) && Utils.pocketGestureEnabled(mContext)) {
+            return true;
+        } else if (Utils.handwaveGestureEnabled(mContext)) {
+            return delta < HANDWAVE_MAX_DELTA_NS;
+        } else if (Utils.pocketGestureEnabled(mContext)) {
+            return delta >= POCKET_MIN_DELTA_NS;
+        }
+        return false;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        /* Empty */
+    }
+
+    protected void enable() {
+        if (DEBUG) Log.d(TAG, "Enabling");
+        mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void disable() {
+        if (DEBUG) Log.d(TAG, "Disabling");
+        mSensorManager.unregisterListener(this, mSensor);
+    }
+}

src/com/blackbox/settings/fragments/doze/TiltSensor.java
@@ -1,0 +1,79 @@
+/*
+ * Copyright (C) 2017 BlackBox Hybrid Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.blackbox.settings.fragments.doze;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.SystemClock;
+import android.util.Log;
+
+public class TiltSensor implements SensorEventListener {
+
+    private static final boolean DEBUG = false;
+    private static final String TAG = "TiltSensor";
+
+    private static final int BATCH_LATENCY_IN_MS = 100;
+    private static final int MIN_PULSE_INTERVAL_MS = 2500;
+
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+    private Context mContext;
+
+    private long mEntryTimestamp;
+
+    public TiltSensor(Context context) {
+        mContext = context;
+        mSensorManager = (SensorManager) mContext.getSystemService(Context.SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_TILT_DETECTOR);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (DEBUG) Log.d(TAG, "Got sensor event: " + event.values[0]);
+
+        long delta = SystemClock.elapsedRealtime() - mEntryTimestamp;
+        if (delta < MIN_PULSE_INTERVAL_MS) {
+            return;
+        } else {
+            mEntryTimestamp = SystemClock.elapsedRealtime();
+        }
+
+        if (event.values[0] == 1) {
+            Utils.launchDozePulse(mContext);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        /* Empty */
+    }
+
+    protected void enable() {
+        if (DEBUG) Log.d(TAG, "Enabling");
+        mSensorManager.registerListener(this, mSensor,
+                SensorManager.SENSOR_DELAY_NORMAL, BATCH_LATENCY_IN_MS * 1000);
+        mEntryTimestamp = SystemClock.elapsedRealtime();
+    }
+
+    protected void disable() {
+        if (DEBUG) Log.d(TAG, "Disabling");
+        mSensorManager.unregisterListener(this, mSensor);
+    }
+}

src/com/blackbox/settings/fragments/doze/Utils.java
@@ -1,0 +1,89 @@
+/*
+ * Copyright (C) 2017 BlackBox Hybrid Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.blackbox.settings.fragments.doze;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.util.Log;
+
+import static android.provider.Settings.Secure.DOZE_ENABLED;
+
+public final class Utils {
+
+    public static final Uri DOZE_ENABLED_URI = Settings.Secure.getUriFor(DOZE_ENABLED);
+    private static final String TAG = "DozeUtils";
+    private static final boolean DEBUG = false;
+    private static final String DOZE_INTENT = "com.android.systemui.doze.pulse";
+
+    protected static void startService(Context context) {
+        if (DEBUG) Log.d(TAG, "Starting service");
+        context.startService(new Intent(context, DozeService.class));
+    }
+
+    protected static void stopService(Context context) {
+        if (DEBUG) Log.d(TAG, "Stopping service");
+        context.stopService(new Intent(context, DozeService.class));
+    }
+
+    protected static boolean isDozeEnabled(Context context) {
+        return Settings.Secure.getInt(context.getContentResolver(),
+                DOZE_ENABLED, context.getResources().getBoolean(
+                com.android.internal.R.bool.config_doze_enabled_by_default) ? 1 : 0) != 0;
+    }
+
+    protected static void enableService(boolean enable, Context context) {
+        if (enable) {
+            startService(context);
+        } else {
+            stopService(context);
+        }
+    }
+
+    protected static void launchDozePulse(Context context) {
+        if (DEBUG) Log.d(TAG, "Launch doze pulse");
+        context.sendBroadcastAsUser(new Intent(DOZE_INTENT),
+                new UserHandle(UserHandle.USER_CURRENT));
+    }
+
+    protected static boolean tiltEnabled(Context context) {
+        return Settings.System.getInt(context.getContentResolver(),
+                Settings.System.DOZE_TRIGGER_TILT, 0) == 1;
+    }
+
+    protected static boolean pickUpEnabled(Context context) {
+        return Settings.System.getInt(context.getContentResolver(),
+                Settings.System.DOZE_TRIGGER_PICKUP, 0) == 1;
+    }
+
+    protected static boolean handwaveGestureEnabled(Context context) {
+        return Settings.System.getInt(context.getContentResolver(),
+                Settings.System.DOZE_TRIGGER_HANDWAVE, 0) == 1;
+    }
+
+    protected static boolean pocketGestureEnabled(Context context) {
+        return Settings.System.getInt(context.getContentResolver(),
+                Settings.System.DOZE_TRIGGER_POCKET, 0) == 1;
+    }
+
+    protected static boolean sensorsEnabled(Context context) {
+        return tiltEnabled(context) || pickUpEnabled(context) || handwaveGestureEnabled(context)
+                || pocketGestureEnabled(context);
+    }
+}