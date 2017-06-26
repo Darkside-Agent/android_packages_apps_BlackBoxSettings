crdroidandroid/android_packages_apps_crDroidSettings

Fix NPE of backlight settings [1/2]
Change-Id: I5296325457acee15da587b199113528c1ca7cd43
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/com/crdroid/settings/fragments/ButtonBacklightBrightness.java
@@ -18,12 +18,12 @@ import android.widget.CompoundButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
-import com.crdroid.settings.preferences.CustomDialogPreference;
+import com.crdroid.settings.preferences.CustomDialogPref;
 import com.android.settings.R;
 
 import cyanogenmod.providers.CMSettings;
 
-public class ButtonBacklightBrightness extends CustomDialogPreference<AlertDialog> implements
+public class ButtonBacklightBrightness extends CustomDialogPref<AlertDialog> implements
         SeekBar.OnSeekBarChangeListener {
     private static final int DEFAULT_BUTTON_TIMEOUT = 5;
 

Darkside-Agent/android_packages_apps_BlackBoxSettings

src/com/blackbox/settings/fragments/ButtonBacklightBrightness.java
@@ -18,12 +18,13 @@ import android.widget.CompoundButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
-import com.blackbox.settings.preferences.CustomDialogPreference;
+import com.blackbox.settings.preferences.CustomDialogPref;
+
 import com.android.settings.R;
 
 import cyanogenmod.providers.CMSettings;
 
-public class ButtonBacklightBrightness extends CustomDialogPreference<AlertDialog> implements
+public class ButtonBacklightBrightness extends CustomDialogPref<AlertDialog> implements
         SeekBar.OnSeekBarChangeListener {
     private static final int DEFAULT_BUTTON_TIMEOUT = 5;
 